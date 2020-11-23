42a7e0bcc0f019afd14cd71109a879291996594e
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-11-23 10:28:39 +0900

    buttonのcss

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,22 +1,25 @@
 import * as React from 'react'
+import '../App.css'
 import firebase, { providerTwitter } from '../auth/Firebase'
 
 const Auth = firebase.auth()
 
 export default class Login extends React.Component {
     render () {
+        
         return (
             <div>
-                <button onClick={this.login}>Twitterでログイン</button>
+                <span className="button" onClick={this.login}>Login with Twitter</span>
             </div>
         )
     }
 
     login = () => {
         Auth.signInWithPopup(providerTwitter).then(res => {
-            console.log(res)
+            console.log(res.credential)
+            console.log(res.additionalUserInfo)
         }).catch(err=>{
-            console.log(err.code)
+            console.log(err)
         }) 
     }
     
