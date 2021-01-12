491f46ac9bdc243358fac6d483d3df1e52067fb2
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-01-12 15:03:46 +0900

    mapページからtopへの遷移

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "../App.css";
+import { Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const Map: React.FC = () => {
   return (
     <div className="App">
-      <div className="App-header"></div>
+      <div className="App-header">
+          <p>ISSの位置を確認する
+          </p>
+
+          <Grid container>
+              <Grid item xs={4}>
+                  <Link to="/"> 
+                  <p>topページに戻る</p>
+                  </Link>
+              </Grid>
+              <Grid item xs={8}/>
+          </Grid>
+      </div>
     </div>
   );
 };
