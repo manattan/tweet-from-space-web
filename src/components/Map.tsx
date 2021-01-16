8fe8e05637d20a700252ca4c838a49c39d1ec3f3
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-01-17 01:30:08 +0900

    apply world map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,7 +8,7 @@ const Map: React.FC = () => {
     <div className="App">
       <div className="App-header">
         <p>ISSの位置を確認する</p>
-
+        <img src="/worldmap.jpg" alt="" style={{maxWidth: 800, margin: '0 auto'}}/>
         <Grid container style={{ height: 80 }}>
           <Grid item xs={4}>
             <p style={{ fontSize: 18 }}>topページに戻る</p>
